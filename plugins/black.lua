38416f826d346e238738c0d5cbb955da7ee5cc39
Kazuya-Labs <zuyabizon01@gmail.com>
2025-03-22 11:54:59 +0700

    backup lazyvim

---
+++ plugins/black.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+            require("null-ls").setup {
+                sources = {
+                    require("null-ls").builtins.formatting.black,
+                },
+            }
+        end,
+    },
+}
+
